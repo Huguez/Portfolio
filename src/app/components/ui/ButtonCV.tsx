ce2f2411bb8ee70a37c49dab95c204198652b4d3
huguez <carlos.huguez@gmail.com>
2024-09-17 21:42:15 -0700

    download CV

--- src/app/components/ui/ButtonCV.tsx
+++ src/app/components/ui/ButtonCV.tsx
@@ -1,8 +1,13 @@
 
+
 export default function ButtonCV() {
    
    return <>
-      <a href="#" className="outline-none font-bold text-white bg-blue-600 hover:bg-blue-800 rounded-lg text-sm py-2 px-4  ">
+      <a
+         href="/files/Resume_Carlos_Huguez.pdf"
+         download="Resume_Carlos_Huguez"
+         className="outline-none font-bold text-white bg-blue-600 hover:bg-blue-800 rounded-lg text-sm py-2 px-4  "
+      >
          Download CV
       </a>
    </>
